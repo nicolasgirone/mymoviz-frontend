576af547e4b52becdae23458bbdb15b8810e3b4e
Your name <Your email>
2023-02-01 12:56:46 +0100

    yes

--- components/Home.js
+++ components/Home.js
@@ -12,7 +12,7 @@ function Home() {
   const [moviesData, setMoviesData] = useState([]);
   
   useEffect(() => {
-  fetch('https://mymoviz-part5-backend-rho.vercel.app/movies')
+  fetch('https://mymoviz-backend-omega.vercel.app/movies')
     .then(response => response.json())
     .then(data => {
       const formatedData = data.movies.map(movie => {
